930ddfd8957ec56767c87a426d7a91b698425d22
Piotr <prusinski.pio@gmail.com>
2018-02-06 13:25:39 +0100

    ch12 census program, csv files added

--- ch12census.py
+++ ch12census.py
@@ -1,2 +1,32 @@
+#!/usr/bin/python
+# Tabulates population and number of census tracts for each county
+
 import openpyxl
+import pprint
+
+wb = openpyxl.load_workbook('censuspopdata.xlsx')
+sheet = wb['Population by Census Tract']
+countyData = {}
+
+# TODO: Fill in countyData with each county's population and tracts
+
+for row in range(2, sheet.max_row + 1):
+    state = sheet['B' + str(row)].value
+    county = sheet['C' + str(row)].value
+    pop = sheet['D' + str(row)].value
+
+    countyData.setdefault(state, {})
+    countyData[state].setdefault(county, {'tracts': 0, 'pop': 0})
+
+    countyData[state][county]['tracts'] += 1
+    # Increase the county pop by the pop in this census tract
+    countyData[state][county]['pop'] += int(pop)
+
+# TODO: Open a new text file and write the contents of countyData to iter
+resultFile = open('census2010.py', 'w')
+resultFile.write('allData = ' + pprint.pformat(countyData))
+resultFile.close()
+print('Done.')
+
+
 
